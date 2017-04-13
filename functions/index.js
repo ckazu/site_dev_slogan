3c8d449cefb1b68811298aa5e9236c4e1a04e137
kchinda <kchinda@aiming-inc.com>
2017-04-14 01:19:44 +0900

    Add script

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,49 @@
-var functions = require('firebase-functions');
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const _ = require('underscore');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+
+exports.slogan = functions.https.onRequest((req, res) => {
+    const db = admin.database();
+    const ref = db.ref("/slogan");
+
+    if(_.isEmpty(req.body.text)) {
+        ref.limitToFirst(100).once('value', (snapshot) => {
+            res.json({response_type: "in_channel", text: _.sample(snapshot.val())});
+        });
+    } else {
+        switch (true) {
+        case /^add[\s]+.*$/.test(req.body.text):
+            const [_add, text] = req.body.text.match(/add[\s]*(.*)$/);
+            ref.push().set(text);
+
+            ref.limitToFirst(100).once('value', (snapshot) => {
+                const list = _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
+                res.json({response_type: "in_channel", text: "追加しました\n" + list});
+            });
+            break;
+        case /^list[\s]*$/.test(req.body.text):
+            ref.limitToFirst(100).once('value', (snapshot) => {
+                res.json({
+                    response_type: "in_channel",
+                    text: _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
+                });
+            });
+            break;
+        case /^remove[\s]+.*$/.test(req.body.text):
+            const [_remove, key] = req.body.text.match(/remove[\s]*(.*)$/);
+
+            ref.child(key).remove();
+            ref.limitToFirst(100).once('value', (snapshot) => {
+                const list = _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
+                res.json({response_type: "in_channel", text: list});
+            });
+
+            break;
+        default:
+            res.status(404).end('error!: ' + req.body.text);
+            break;
+        }
+    }
+});
