f8e0d143b6c1cd551b73a1e3615e87475774d381
kchinda <kchinda@aiming-inc.com>
2017-04-19 13:09:18 +0900

    Use intaractive button

--- functions/index.js
+++ functions/index.js
@@ -4,43 +4,61 @@ const _ = require('underscore');
 
 admin.initializeApp(functions.config().firebase);
 
+const formattedAttachments = (key, value) => {
+    return {
+        callback_id: "remove",
+        text: value,
+        actions: [{
+            name: "remove",
+            text: "削除",
+            type: "button",
+            style: "danger",
+            value: key,
+            confirm: {
+                title: "確認",
+                text: "このデータを削除しますか？",
+                ok_text: "はい",
+                dismiss_text: "いいえ"
+            }
+        }]
+    }
+}
+
 exports.slogan = functions.https.onRequest((req, res) => {
     const db = admin.database();
     const ref = db.ref("/slogan");
 
-    if(_.isEmpty(req.body.text)) {
+    if(req.body.payload) {
+        const payload = JSON.parse(req.body.payload);
+        if (payload.actions[0].name == "remove") {
+            ref.child(payload.actions[0].value).remove();
+
+            ref.limitToFirst(100).once('value', (snapshot) => {
+                const attachments = _.map(snapshot.val(), (value, key) => { return formattedAttachments(key, value); });
+                res.json({response_type: "ephemeral", text: "削除しました", attachments: attachments});
+            });
+        }
+
+    } else if(_.isEmpty(req.body.text)) {
         ref.limitToFirst(100).once('value', (snapshot) => {
             res.json({response_type: "in_channel", text: _.sample(snapshot.val())});
         });
+
     } else {
         switch (true) {
         case /^add[\s]+.*$/.test(req.body.text):
-            const [_add, text] = req.body.text.match(/add[\s]*(.*)$/);
+            const text = req.body.text.match(/add[\s]*(.*)$/)[1];
             ref.push().set(text);
-
-            ref.limitToFirst(100).once('value', (snapshot) => {
-                const list = _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
-                res.json({response_type: "in_channel", text: "追加しました\n" + list});
-            });
+            res.json({response_type: "ephemeral", text: "`" + text + "`を追加しました"});
             break;
+
         case /^list[\s]*$/.test(req.body.text):
             ref.limitToFirst(100).once('value', (snapshot) => {
-                res.json({
-                    response_type: "in_channel",
-                    text: _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
-                });
+                const attachments = _.map(snapshot.val(), (value, key) => { return formattedAttachments(key, value); });
+                res.json({response_type: "ephemeral", attachments: attachments});
             });
             break;
-        case /^remove[\s]+.*$/.test(req.body.text):
-            const [_remove, key] = req.body.text.match(/remove[\s]*(.*)$/);
 
-            ref.child(key).remove();
-            ref.limitToFirst(100).once('value', (snapshot) => {
-                const list = _.map(snapshot.val(), function(v, k) { return '[' + k + ']: ' + v}).join("\n")
-                res.json({response_type: "in_channel", text: list});
-            });
-
-            break;
         default:
             res.status(404).end('error!: ' + req.body.text);
             break;
